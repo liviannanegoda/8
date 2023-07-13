fab37f2e89c6b0ec97dde771f099524bfc0b0230
unknown <lidianegoda5@gmail.com>
2023-07-13 12:55:53 +0300

    Добавила задачи

---
+++ задача 2/Program.cs
@@ -0,0 +1,62 @@
+﻿//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка.
+using System;
+using static System.Console;
+
+Clear();
+
+int[,] array = new int[3, 4];
+GetArray(array);
+PrintArray(array);
+WriteLine();
+GetRowNumber(array);
+
+void GetRowNumber(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Write($"{minSumRow + 1} строка с наименьшей суммой элементов");
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
---
+++ задача 6/Program.cs
@@ -0,0 +1,83 @@
+﻿//задача 59. Из двумерного массива целых чисел удалить строку и столбец, на пересечении которых расположен наименьший элемент.
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+
+PrintArray(array);
+WriteLine();
+WriteLine($"Минимальный элемент находится по индексам {String.Join(" ", GetIndexMinEl(array))}");
+WriteLine();
+PrintArray(GetResultArray(array, GetIndexMinEl(array)));
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+int[] GetIndexMinEl(int[,] inArray)
+{
+    int[] result = new int[] { 0, 0 };
+    int min = inArray[0, 0];
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (min > inArray[i, j])
+            {
+                min = inArray[i, j];
+                result[0] = i;
+                result[1] = j;
+            }
+        }
+    }
+    return result;
+}
+int[,] GetResultArray(int[,] inArray, int[] index)
+{
+    int[,] result = new int[inArray.GetLength(0) - 1, inArray.GetLength(1) - 1];
+    int row = 0;
+    int column = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        if (i == index[0])
+            continue;
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (j == index[1])
+                continue;
+            result[row, column] = inArray[i, j];
+            column++;
+        }
+        column = 0;
+        row++;
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
